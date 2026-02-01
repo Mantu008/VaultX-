2ce2336ac23adaa5771e3beeb3433548766e4cf7
mantu <mantukumar87586299@gmail.com>
2026-02-01 20:01:34 +0530

    about page chenge

--- Client/src/components/About.jsx
+++ Client/src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <p>This is the about page of our application.</p>
             <p>Here you can find information about our team and the purpose of this project.</p>
             <p>We aim to provide the best user experience and valuable content.</p>
-            <p>Thank you for visiting!</p>
+            <p>Thank you for visiting.</p>
         </div>
     )
 }
